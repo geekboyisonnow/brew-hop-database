73085931df7b9e3fdd5b63342172f13984b7ac50
Kenneth Cannon <kennethfcannon@gmail.com>
2019-01-10 17:21:56 -0500

    Count # of Ratings for each Kind of Bar

--- app/models/brewery_rating.rb
+++ app/models/brewery_rating.rb
@@ -1,3 +1,20 @@
 class BreweryRating < ApplicationRecord
-    belongs_to :bar
+  belongs_to :bar
+
+  def overall_rating
+    [
+        ipa,
+        apa,
+        ale,
+        hefe,
+        porter,
+        stout,
+        pale,
+        blonde,
+        pilsner,
+        growler,
+        crowler,
+        rail
+    ].count(true)
+  end
 end
