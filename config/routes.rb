fb5892eb94fd56d4c1c4d14adc0370e37d5664e5
Kenneth Cannon <kennethfcannon@gmail.com>
2019-01-15 12:27:13 -0500

    Set up for adding App into Rails

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   resources :bars
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  if Rails.env.production?
+    CLIENT_HTML = File.read(Rails.root.join('public/index.html'))
+
+    get "*path", to: proc { [200, {}, [CLIENT_HTML]] }
+  end
 end
